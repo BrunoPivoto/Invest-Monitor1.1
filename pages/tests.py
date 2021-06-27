71acd1ff777c8cbbbdfb12f3d0dc9a55592e9886
Bruno Pivoto <brunorangel@gec.inatel.br>
2021-06-27 20:26:01 -0300

    tentativa de teste

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
-
+from .models import Stock
 # Create your tests here.
+
+class StockTestCase(TestCase):
+    def setUp(self):
+        Stock.objects.create(Codigo="ITUB4", Preço=45, Quantidade=10, Data=2021-12-27 )
+
+    def test_stock_date(self):
+        
+        itau = Stock.objects.get(name="ITUB4")
+        self.assertEqual(itau.Data, 2021-12-27)
